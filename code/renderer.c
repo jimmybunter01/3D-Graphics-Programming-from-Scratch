d1edb847c5c8d774b6e2038c23c4351d429ef388
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2024-09-14 18:12:43 +0100

    We are now rotating a cube of points. After adding functions to rotate a 3 dimensional vector along the x,y and z axes.

--- code/renderer.c
+++ code/renderer.c
@@ -14,6 +14,7 @@
 vec3_t cube_points[NO_POINTS];
 vec2_t projected_points[NO_POINTS];
 vec3_t camera_position = {.x=0, .y=0, .z=-5};
+vec3_t cube_rotation = {.x=0, .y=0, .z=0};
 
 float fov_factor = 640; // Magic Number for now!
 bool is_running = false;
@@ -71,13 +72,21 @@ vec2_t naive_orthographic_projection(vec3_t point) {
 }
 
 void update() {
+    cube_rotation.x += 0.01;
+    cube_rotation.y += 0.01;
+    cube_rotation.z += 0.01;
+    
     for (int i=0; i < NO_POINTS; i++) {
         vec3_t point = cube_points[i];
 
-        // We need to do this so that the point are moved away from the camera.
-        point.z -= camera_position.z;
+        vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);    
+        transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+        transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
         
-        vec2_t projected_point = naive_orthographic_projection(point);
+        // Translate points away from the camera
+        transformed_point.z -= camera_position.z;
+        
+        vec2_t projected_point = naive_orthographic_projection(transformed_point);
         projected_points[i] = projected_point;
     }
 }
--- code/vector.c
+++ code/vector.c
@@ -1,1 +1,31 @@
 #include "vector.h"
+#include <math.h>
+
+vec3_t vec3_rotate_x(vec3_t v, float angle) {
+  vec3_t rotated_vector = {
+      .x = v.x,
+      .y = v.y * cos(angle) - v.z * sin(angle),
+      .z = v.z * cos(angle) + v.y * sin(angle),
+  };
+  return rotated_vector;
+}
+
+
+vec3_t vec3_rotate_y(vec3_t v, float angle) {
+  vec3_t rotated_vector = {
+      .x = v.x * cos(angle) - v.z * sin(angle),
+      .y = v.y,
+      .z = v.z * cos(angle) + v.x * sin(angle)
+  };
+  return rotated_vector; 
+}
+
+
+vec3_t vec3_rotate_z(vec3_t v, float angle) {
+  vec3_t rotated_vector = {
+      .x = v.x * cos(angle) - v.y * sin(angle),
+      .y = v.y * cos(angle) + v.x * sin(angle),
+      .z = v.z
+  };
+  return rotated_vector;
+}
